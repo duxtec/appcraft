f7c518f4212d59427c1dc7c10705a1443c96037d
Dux <dux.tec.br@gmail.com>
2025-02-24 09:47:43 -0300

    refactor: reorganized domain interfaces and exceptions

--- appcraft/templates/base/files/domain/adapters/app_adapter_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from domain.models.app import App
-
-
-class AppAdapterInterface(ABC):
-    @abstractmethod
-    def get_app(self) -> App:
-        pass
--- appcraft/templates/base/files/domain/interfaces/repositories/user_repository.py
+++ appcraft/templates/base/files/domain/interfaces/repositories/user_repository.py
@@ -1,19 +1,21 @@
 from abc import ABC, abstractmethod
+from typing import List
 
+from domain.filters import FilterInterface
 from domain.models.user import User
 
 
 class UserRepositoryInterface(ABC):
     @abstractmethod
-    def get_by_id(self, user_id: int) -> User:
+    def get(self, filters: List[FilterInterface]) -> List[User]:
         pass
 
     @abstractmethod
-    def create(self, user: User) -> None:
+    def create(self, user: User) -> User:
         pass
 
     @abstractmethod
-    def update(self, user: User) -> None:
+    def update(self, user: User) -> User:
         pass
 
     @abstractmethod
